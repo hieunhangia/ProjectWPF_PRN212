6db0388062fb788758c2d72cab18a9351f09e8aa
HieuNhanGia <HieuNhanGia@LAPTOP36>
2025-10-03 16:31:50 +0700

    Add project files.

---
+++ ProjectWPF/Login.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (emailTextBox.Text != "quetoi36" || passwordBox.Password != "suthanhhoa")
+            {
+                MessageBox.Show("Sai cmnr");
+            }
+            else
+            {
+                MessageBox.Show("Chuẩn cmnr");
+            }
+        }
+    }
+}
