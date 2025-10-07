c38034a6af378ef42d630ab977a3cbcaa82c22a3
hieunhangia <hieuvippro531@gmail.com>
2025-10-08 03:54:47 +0700

    update validate for seller infomation field

--- ProjectWPF/AdminWindows/AddSeller.xaml.cs
+++ ProjectWPF/AdminWindows/AddSeller.xaml.cs
@@ -3,7 +3,9 @@ using Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -75,14 +77,52 @@ namespace ProjectWPF.AdminWindows
                 communneWard == "default" ||
                 string.IsNullOrWhiteSpace(specificAddress))
             {
-                MessageBox.Show("Hãy điền đầy đủ thông tin.", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Hãy cung cấp đầy đủ thông tin.", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
             else
             {
-                if(birthDate > DateOnly.FromDateTime(DateTime.Now))
+                static bool IsValidEmail(string email)
+                {
+                    try
+                    {
+                        var addr = new MailAddress(email);
+                        return addr.Address == email;
+                    }
+                    catch
+                    {
+                        return false;
+                    }
+                }
+                if (!IsValidEmail(email))
+                {
+                    MessageBox.Show("Email không hợp lệ.", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                static bool IsValidPassword(string password) =>
+                    password.Length >= 6
+                    && password.Length <= 50
+                    && password.Any(char.IsLower)
+                    && password.Any(char.IsUpper)
+                    && password.Any(char.IsDigit)
+                    && password.Any(ch => !char.IsLetterOrDigit(ch));
+                if (!IsValidPassword(password))
+                {
+                    MessageBox.Show("Mật khẩu phải trong khoảng 6-50 ký tự, bao gồm chữ hoa, chữ thường, số và ký tự đặc biệt.", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                if (birthDate > DateOnly.FromDateTime(DateTime.Now))
                 {
                     MessageBox.Show("Ngày sinh không hợp lệ.", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                static bool IsValidID(string identify) => (identify.Length == 9 || identify.Length == 12) && identify.All(char.IsDigit);
+                if (!IsValidID(identify))
+                {
+                    MessageBox.Show("CMND/CCCD không hợp lệ.", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
                     return;
                 }
 
