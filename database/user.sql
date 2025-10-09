1f3a85ed904dde429783c1f25a7dc850eee7e1fe
hieunhangia <hieuvippro531@gmail.com>
2025-10-10 02:27:59 +0700

    update database #2

--- database/user.sql
+++ database/user.sql
@@ -2,10 +2,10 @@
 insert into admin values ((select id from [user] where email = 'admin@shop.com'))
 
 INSERT INTO [dbo].[user] ([email], [password], [enabled]) VALUES ('s1@shop.com', '@0Seller', 1);
-INSERT INTO [dbo].[seller] VALUES ((SELECT id FROM [dbo].[user] WHERE email = 's1@shop.com'), N'Hoa Thanh Quế', '2004-05-15', '123456789', N'Thủ Đô Linh Thiêng', '16279');
+INSERT INTO [dbo].[seller] VALUES ((SELECT id FROM [dbo].[user] WHERE email = 's1@shop.com'), N'Hoá Thanh Sư', '2004-05-15', '363636363636', N'Thủ Đô Linh Thiêng', '16279');
 
 INSERT INTO [dbo].[user] ([email], [password], [enabled]) VALUES ('s2@shop.com', '@0Seller', 1);
-INSERT INTO [dbo].[seller] VALUES ((SELECT id FROM [dbo].[user] WHERE email = 's2@shop.com'), N'Trà Từ Tay', '2003-11-20', '987654321', N'Ngõ 120', '08980');
+INSERT INTO [dbo].[seller] VALUES ((SELECT id FROM [dbo].[user] WHERE email = 's2@shop.com'), N'Trà Từ Tay', '2003-11-20', '120120120', N'Ngõ 120', '08980');
 
 INSERT INTO [dbo].[user] ([email], [password], [enabled]) VALUES ('s3@shop.com', '@0Seller', 1);
-INSERT INTO [dbo].[seller] VALUES ((SELECT id FROM [dbo].[user] WHERE email = 's3@shop.com'), N'Khả Seni', '2006-01-01', '567891234', N'Bệnh Viện', '11443');
+INSERT INTO [dbo].[seller] VALUES ((SELECT id FROM [dbo].[user] WHERE email = 's3@shop.com'), N'Khả Se Ni', '2006-01-01', '888888888', N'Bệnh Viện', '11443');
